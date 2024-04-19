ecd6b6d1cb8b56337da392c75d6c1d09bcc7da1c
Lisawakhidah <wakhidahlisa@gmail.com>
2024-04-19 10:44:18 +0700

    padding

--- projek_flutter_ui/lib/main.dart
+++ projek_flutter_ui/lib/main.dart
@@ -10,6 +10,7 @@ import 'package:projek_flutter_ui/layout/column.dart';
 import 'package:projek_flutter_ui/layout/gridviewbuilder.dart';
 import 'package:projek_flutter_ui/layout/listview.dart';
 import 'package:projek_flutter_ui/layout/listviewbuilder.dart';
+import 'package:projek_flutter_ui/layout/padding.dart';
 import 'package:projek_flutter_ui/layout/row.dart';
 import 'package:projek_flutter_ui/layout/stack.dart';
 
@@ -45,7 +46,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyStack(),
+      home: const MyPadding(),
     );
   }
 }
